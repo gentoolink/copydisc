a72bc9bceca62d74847e3cbd5c1d44eb409f7830
Ken McGonigal <ken.mcgonigal@gmail.com>
2016-08-10 13:12:22 -0700

    update
    cleared white space

--- copydisc.cmd
+++ copydisc.cmd
@@ -3,9 +3,6 @@
 REM This file is for copying bus CCTV without using the windows explorer window. Windows explorer takes too long to load
 REM the files. This will work for any cdrom/dvd containing files to copy to a PC. 
 
-
-
-
 set /p filenumber="Enter Case file number: "
 
 REM create directory with file number as name
